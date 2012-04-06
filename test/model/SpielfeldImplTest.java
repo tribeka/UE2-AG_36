11c75d729e4607554973fb2e238cba50736a8661
Bruno Bajtela <Bruno Bajtela@brb>
2012-04-06 11:08:50 +0200

    Weiterer Testfall

--- test/model/SpielfeldImplTest.java
+++ test/model/SpielfeldImplTest.java
@@ -116,6 +116,32 @@ public class SpielfeldImplTest {
         
         assertTrue(f.isPlayerFinished(s1));
     }
+    @Test
+    public void DistanceAndFinishTest_Green()
+    {
+        List<Spieler> players = new ArrayList<Spieler>();
+        
+        Spieler s1 = new Spieler("Test1");
+        
+        players.add(new Spieler("Test2"));
+        players.add(s1);
+        
+        Spielfeld f = new SpielfeldImpl(players);
+        
+        f.setPlayerToField(s1, f.getNewField(s1, 1));
+        
+        assertEquals(new Integer(40), f.distanceToFinish(s1));
+        
+        Feld fe = f.getNewField(s1, 39);
+        
+        assertTrue(fe.isLastField());
+        f.setPlayerToField(s1, fe);
+        assertFalse(f.isPlayerFinished(s1));
+        
+        f.setPlayerToField(s1, f.getNewField(s1, 1));
+        
+        assertTrue(f.isPlayerFinished(s1));
+    }
     
 
 }
